bccd87f676893aca0382e09e6a75e046c78c3fe1
Alberto Villegas <alvich@floridauniversitaria.es>
2023-10-11 20:37:42 +0200

    Guardando mas cambios de AE2

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@
  * @format
  */
 
+import { useState } from 'react';
 import React from 'react';
 import {
   StatusBar,
@@ -14,18 +15,16 @@ import {
   TextInput,
   ImageBackground,
   SafeAreaView,
-  Button,
-  Switch
+  View,
 } from 'react-native';
 
-//import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // Importa el componente Icon
 
-import { Chip } from 'react-native-paper';
+import { Chip, Button, Surface, Switch } from 'react-native-paper';
 
 const image = { uri: 'https://legacy.reactjs.org/logo-og.png' };
 
 const App = () => {
-  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
 
   return (
@@ -46,21 +45,42 @@ const App = () => {
           placeholder="Email"
         />
         <Text style={estils.text}>Button(amb text i icona)</Text>
-
-
+        <Button icon="alien" mode="contained" onPress={() => console.log('Pressed')} style={estils.button}>
+          Alien
+        </Button>
+        <Button icon="alien" mode="contained" onPress={() => console.log('Pressed')} style={estils.button}>
+          ALIEN
+        </Button>
+        <Button icon="alien" mode="contained" onPress={() => console.log('Pressed')} style={estils.button}>
+          ALIEN
+        </Button>
+        <Button icon="alien" mode="contained" onPress={() => console.log('Pressed')} style={estils.button} >
+          ALIEN
+        </Button>
         <Text style={estils.text}>Switch Necessites un descans</Text>
         <Switch
+          color="red"
           onValueChange={onToggleSwitch}
           value={isSwitchOn}
         />
         <Text style={estils.text}>Botó dins un d`component
           Surface?</Text>
-        <Button title="Press me" />
+        <Surface style={estils.surface} elevation={4}>
+          <Button icon="alien" mode="contained" onPress={() => console.log('Pressed')} style={estils.button}>
+            ALIEN
+          </Button>
+        </Surface>
         <Text style={estils.text}>Provant Chips</Text>
-        <Chip icon="internet" onPress={() => console.log('Pressed')} style={estils.chip1} textStyle={estils.chipText}>Internet</Chip>
-        <Chip icon="Wifi" onPress={() => console.log('Pressed')} style={estils.chip2} textStyle={estils.chiptext2}>Wifi</Chip>
+        <View style={{ flexDirection: 'row' }}>
+          <Chip icon="web" onPress={() => console.log('Pressed')} style={estils.chip1}>
+            Internet
+          </Chip>
+          <Chip icon="wifi" onPress={() => console.log('Pressed')} style={estils.chip2}>
+            Wifi
+          </Chip>
+        </View>
       </SafeAreaView>
-    </ScrollView>
+    </ScrollView >
   );
 };
 
@@ -70,6 +90,13 @@ const estils = StyleSheet.create({
   },
   backgroundImage: {
     height: 270,
+  },
+  surface: {
+    padding: 8,
+    height: 80,
+    width: 450,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   sectionTitle: {
     fontSize: 55,
@@ -89,10 +116,18 @@ const estils = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
   },
+  button: {
+    marginBottom: 10,
+    backgroundColor: 'black',
+  },
   text: {
     fontSize: 27,
     color: 'white',
     marginTop: 40,
+  },
+  buttonAlien: {
+    backgroundColor: 'blue',
+    color: 'yellow'
   },
   safeAreaView: {
     flex: 1,
@@ -103,20 +138,20 @@ const estils = StyleSheet.create({
     padding: 8,
     borderRadius: 16,
     margin: 4,
-    width: 140,
+    width: 120,
   },
   chipText: {
     color: 'gray',
   },
   chiptext2: {
-    color: 'red',
+    color: 'gray',
   },
   chip2: {
     backgroundColor: 'white',
     padding: 8,
     borderRadius: 16,
     margin: 4,
-    width: 140,
+    width: 100,
   },
 });
 
