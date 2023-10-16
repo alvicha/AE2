ed94850b25437e1e6664bff23f5f5df1049d1806
Alberto Villegas <alvich@floridauniversitaria.es>
2023-10-16 19:36:37 +0200

    Colocando ejemplos de JSX tema nuevo

--- App.tsx
+++ App.tsx
@@ -8,78 +8,80 @@
 import React from 'react';
 import {
   StatusBar,
-  ScrollView,
   StyleSheet,
   Text,
-  TextInput,
-  ImageBackground,
-  SafeAreaView,
-  Button,
-  Switch
+  View,
 } from 'react-native';
 
-//import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // Importa el componente Icon
+import { Provider as PaperProvider, TextInput } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { Chip } from 'react-native-paper';
-
-const image = { uri: 'https://legacy.reactjs.org/logo-og.png' };
-
-const App = () => {
-  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
-  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
-
+const nom = (textAMostrar) => {
   return (
-    <ScrollView style={estils.container}>
-      <ImageBackground
-        source={image}
-        style={estils.backgroundImage}
-      >
-        <Text style={estils.sectionTitle}>Welcome to React Native</Text>
-      </ImageBackground>
-      <SafeAreaView style={estils.safeAreaView}>
-        <Text style={estils.text}>TextInput (email)</Text>
-        <TextInput
-          editable
-          multiline
-          maxLength={40}
-          style={estils.textInput}
-          placeholder="Email"
-        />
-        <Text style={estils.text}>Button(amb text i icona)</Text>
-
-
-        <Text style={estils.text}>Switch Necessites un descans</Text>
-        <Switch
-          onValueChange={onToggleSwitch}
-          value={isSwitchOn}
-        />
-        <Text style={estils.text}>Botó dins un d`component
-          Surface?</Text>
-        <Button title="Press me" />
-        <Text style={estils.text}>Provant Chips</Text>
-        <Chip icon="internet" onPress={() => console.log('Pressed')} style={estils.chip1} textStyle={estils.chipText}>Internet</Chip>
-        <Chip icon="Wifi" onPress={() => console.log('Pressed')} style={estils.chip2} textStyle={estils.chiptext2}>Wifi</Chip>
-      </SafeAreaView>
-    </ScrollView>
+    <View>
+      <Text style={estils.sectionTitle}>Hola {textAMostrar}</Text>
+    </View>
   );
+}
+
+const dades = (arr) => {
+  return (
+    <View>
+      {
+        arr.map((component, index) => {
+          return (
+            <TextInput
+              key={index}
+              label={component}
+              placeholder={component}
+              mode='flat'
+            />
+          );
+        })
+      }
+    </View>
+  )
 };
 
+// El simbolo && (AND) se usa para cuando este true se ponga un mensaje sino si es false se hará otra función.
+const App = () => {
+  let array = ['Email', 'Nom'];
+  return (
+    <PaperProvider>
+      {nom("Alberto Villegas Chaparro")}
+      <SafeAreaView>
+        {dades(array)}
+      </SafeAreaView>
+    </PaperProvider>
+
+  );
+}
+
+
+//{esticLoguejat && Saluda("Benvingut alumne de 2DAM", "Alberto")}
+/*
+const Saluda = (text, nom) => {
+  return (
+    <View>
+      <Text style={estils.sectionTitle}>{text} </Text>
+      <Text style={estils.sectionTitle}>Posicio {nom} </Text>
+    </View>
+  );
+}
+return (
+  <PaperProvider>
+    <StatusBar />
+    {alumnesNom.map((unAlumne, index) => {
+      return Saluda("Benvingut alumne de 2DAM", index + ": " + unAlumne);
+    })}
+  </PaperProvider>
+);
+*/
 const estils = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  backgroundImage: {
-    height: 270,
-  },
   sectionTitle: {
-    fontSize: 55,
-    color: 'white',
-    marginBottom: 20,
+    fontSize: 34,
     fontWeight: 'bold',
     textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 50
   },
   textInput: {
     backgroundColor: '#F0F0F0',
@@ -88,35 +90,6 @@ const estils = StyleSheet.create({
     height: 70,
     margin: 12,
     borderWidth: 1,
-  },
-  text: {
-    fontSize: 27,
-    color: 'white',
-    marginTop: 40,
-  },
-  safeAreaView: {
-    flex: 1,
-    backgroundColor: 'black'
-  },
-  chip1: {
-    backgroundColor: '#F0F0F0',
-    padding: 8,
-    borderRadius: 16,
-    margin: 4,
-    width: 140,
-  },
-  chipText: {
-    color: 'gray',
-  },
-  chiptext2: {
-    color: 'red',
-  },
-  chip2: {
-    backgroundColor: 'white',
-    padding: 8,
-    borderRadius: 16,
-    margin: 4,
-    width: 140,
   },
 });
 
