efb6aea2a25cded668bf334ea8d6b8714dcd1a93
Alberto Villegas <alvich@floridauniversitaria.es>
2023-11-06 19:07:12 +0100

    Agregando cambios

--- App.tsx
+++ App.tsx
@@ -11,69 +11,15 @@ import {
   StyleSheet,
   View,
   Text,
-  TextInput,
 } from 'react-native';
 
+import { Button, TextInput } from 'react-native-paper';
 import CalculadoraIMG from './components/CalculadoraIMG';
-import { Button } from 'react-native-paper';
 
 const App = () => {
-  const [height, setHeight] = useState('');
-  const [weight, setWeight] = useState('');
-  const [resultatIMC, setResultatIMC] = useState(0);
-
-  const calculaIMC = () => {
-    if (weight !== "" && height !== "") {
-      let calculatedIMC = parseFloat(weight) / Math.pow(parseFloat(height), 2);
-      setResultatIMC(calculatedIMC);
-    }
-  }
-
   return (
     <View style={styles.contenidor}>
-      <View style={styles.seccio1}>
-        <Text style={styles.sectionTitle}>CALCULADORA IMC</Text>
-      </View>
-      <View style={styles.seccio2}>
-        <View style={styles.peso}>
-          <View style={styles.seccioPeso}>
-            <Text style={styles.titlePeso}>Peso:</Text>
-          </View>
-          <View style={styles.textInputPeso}>
-            <TextInput
-              style={styles.resultat}
-              onChangeText={setWeight}
-              value={weight}
-              placeholder="Peso"
-            />
-          </View>
-        </View>
-        <View style={styles.seccioAltura}>
-          <View style={styles.altura}>
-            <Text style={styles.titleAltura}>Altura:</Text>
-          </View>
-          <View style={styles.textInputAltura}>
-            <TextInput
-              onChangeText={setHeight}
-              style={styles.resultat}
-              value={height}
-              placeholder="Altura"
-            />
-          </View>
-        </View>
-        <View style={styles.seccioBoto}>
-          <Button mode="contained" onPress={calculaIMC} style={styles.button}>
-            CALCULAR!
-          </Button>
-        </View>
-      </View>
-      <View style={styles.seccio3}>
-        <View style={styles.resultatIMC}>
-          <Text style={styles.IMC
-          }>{resultatIMC}</Text>
-            <CalculadoraIMG valorIMC={resultatIMC} />
-        </View>
-      </View>
+      <CalculadoraIMG />
     </View>
   );
 };
@@ -82,112 +28,7 @@ const styles = StyleSheet.create({
   contenidor: {
     flex: 1, //Lo que hace es que entre los tres divs me hacen en uno solo y que me queden cuadrados con exactitud
     flexDirection: 'column',
-    borderColor: 'purple',
-    borderWidth: 5,
-  },
-  resultat: {
-  },
-  seccioAltura: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 12,
-    alignContent: 'space-between',
-    width: 350,
-    marginTop: 15,
-  },
-  titleAltura: {
-    fontSize: 20,
-    fontWeight: '600',
-    padding: 12,
-    textAlign: 'left',
-  },
-  textInputPeso: {
-    justifyContent: 'flex-start',
-    marginLeft: 5,
-    width: 120,
-  },
-  seccioBoto: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 12,
-    alignContent: 'space-between',
-    width: 350,
-    marginTop: 34,
-  },
-  titlePeso: {
-    fontSize: 20,
-    fontWeight: '600',
-    padding: 12,
-    textAlign: 'left',
-  },
-  textInputAltura: {
-    justifyContent: 'flex-start',
-    marginLeft: 5,
-    width: 120,
-  },
-  seccioPeso: {
-    flexDirection: 'row',
-    width: 90,
-    alignContent: 'space-around',
-  },
-  peso: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    padding: 12,
-    alignContent: 'space-between',
-    width: 350,
-  },
-  altura: {
-  },
-  seccio1: {
-    flex: 0.15,
-    fontSize: 12,
-    fontWeight: '600',
-    margin: 4,
-    padding: 12,
-    textAlign: 'right',
-  },
-  seccio2: {
-    flex: 0.5,
-    fontSize: 12,
-    fontWeight: '600',
-    margin: 4,
-    padding: 12,
-    height: 50,
-    textAlign: 'center',
-  },
-  seccio3: {
-    flex: 0.15,
-    fontSize: 12,
-    fontWeight: '600',
-    margin: 4,
-    padding: 12,
-    textAlign: 'left',
-  },
-  sectionTitle: {
-    fontSize: 35,
-    color: 'blue',
-    fontWeight: '600',
-    padding: 12,
-    textAlign: 'center',
-  },
-  button: {
-    borderRadius: 0,
-    color: 'white',
-    width: 300,
-    height: 40,
-  },
-  resultatIMC: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  IMC: {
-    textAlign: 'center',
-    fontSize: 25,
-    fontWeight: 'bold',
-  },
+  }
 });
 
 export default App;
--- components/CalculadoraIMG.js
+++ components/CalculadoraIMG.js
@@ -6,48 +6,206 @@
  * @flow strict-local
  */
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import { Button } from 'react-native-paper';
 
 const CalculadoraIMG = (props) => {
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [resultatIMC, setResultatIMC] = useState(0);
     const [color, setColor] = useState('');
-    let missatgeResultat = '';
-    if (props.valorIMC < 18.5) {
-        missatgeResultat = 'Peso insuficiente';
-        setColor('green');
-    } else if (props.valorIMC >= 18.5 && props.valorIMC <= 24.9) {
-        missatgeResultat = 'Normopeso';
-        setColor('green');
-    } else if (props.valorIMC >= 25 && props.valorIMC <= 26.9) {
-        missatgeResultat = 'Sobrepeso grado I';
-        setColor('orange');
-    } else if (props.valorIMC >= 27 && props.valorIMC <= 29.9) {
-        missatgeResultat = 'Sobrepeso grado II (preobesidad)';
-        setColor('orange');
-    } else if (props.valorIMC >= 30 && props.valorIMC <= 34.9) {
-        missatgeResultat = 'Obesidad de tipo I';
-        setColor('orange');
-    } else if (props.valorIMC >= 35 && props.valorIMC <= 39.9) {
-        missatgeResultat = 'Obesidad de tipo II';
-        setColor('orange');
-    } else if (props.valorIMC >= 40 && props.valorIMC <= 49.9) {
-        missatgeResultat = 'Obesidad de tipo III (mórbida)';
-        setColor('red');
-    } else {
-        missatgeResultat = 'Obesidad de tipo IV (extrema)';
-        setColor('red');
-    }
+    const [missatgeResultat, setMissatgeResultat] = useState('');
 
+    const calculaIMC = () => {
+        if (weight !== "" && height !== "") {
+            let calculatedIMC = parseFloat(weight) / Math.pow(parseFloat(height), 2);
+            setResultatIMC(calculatedIMC);
+            if (calculatedIMC < 18.5) {
+                setMissatgeResultat('Peso insuficiente');
+                setColor('green');
+            } else if (calculatedIMC <= 24.9) {
+                setMissatgeResultat('Normopeso');
+                setColor('green');
+            } else if (calculatedIMC >= 25 && calculatedIMC <= 26.9) {
+                setMissatgeResultat('Sobrepeso grado I');
+                setColor('orange');
+            } else if (calculatedIMC >= 27 && calculatedIMC <= 29.9) {
+                setMissatgeResultat('Sobrepeso grado II (preobesidad)');
+                setColor('orange');
+            } else if (calculatedIMC >= 30 && calculatedIMC <= 34.9) {
+                setMissatgeResultat('Obesidad de tipo I');
+                setColor('orange');
+            } else if (calculatedIMC >= 35 && calculatedIMC <= 39.9) {
+                setMissatgeResultat('Obesidad de tipo II');
+                setColor('orange');
+            } else if (calculatedIMC >= 40 && calculatedIMC <= 49.9) {
+                setMissatgeResultat('Obesidad de tipo III (mórbida)');
+                setColor('red');
+            } else {
+                setMissatgeResultat('Obesidad de tipo IV (extrema)');
+                setColor('red');
+            }
+        }
+    }
     return (
-        <View style={styles.missatge}>
-            <Text style={{ color: color }}>{missatgeResultat}</Text>
+        <View style={styles.contenidor}>
+            <View style={styles.seccio1}>
+                <Text style={styles.sectionTitle}>CALCULADORA IMC</Text>
+            </View>
+            <View style={styles.seccio2}>
+                <View style={styles.peso}>
+                    <View style={styles.seccioPeso}>
+                        <Text style={styles.titlePeso}>Peso:</Text>
+                    </View>
+                    <View style={styles.textInputPeso}>
+                        <TextInput
+                            style={styles.resultat}
+                            onChangeText={setWeight}
+                            value={weight}
+                            placeholder="Peso"
+                            keyboardType="numeric"
+                        />
+                    </View>
+                </View>
+                <View style={styles.seccioAltura}>
+                    <View style={styles.altura}>
+                        <Text style={styles.titleAltura}>Altura:</Text>
+                    </View>
+                    <View style={styles.textInputAltura}>
+                        <TextInput
+                            onChangeText={setHeight}
+                            style={styles.resultat}
+                            value={height}
+                            placeholder="Altura"
+                            keyboardType="numeric"
+                        />
+                    </View>
+                </View>
+                <View style={styles.seccioBoto}>
+                    <Button mode="contained" onPress={calculaIMC} style={styles.button}>
+                        CALCULAR!
+                    </Button>
+                </View>
+            </View>
+            <View style={styles.seccio3}>
+                <View style={styles.resultatIMC}>
+                    <Text style={styles.IMC}>{resultatIMC}</Text>
+                    <Text style={{ fontSize: 25, color: color }}>{missatgeResultat}</Text>
+                </View>
+            </View>
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
-    missatge: {
+    contenidor: {
+        flex: 1,
+        flexDirection: 'column',
+        borderColor: 'purple',
+        borderWidth: 5,
+    },
+    resultat: {
+    },
+    seccioAltura: {
+        flexDirection: 'row',
         justifyContent: 'center',
-        alignSelf: 'center'
+        alignItems: 'center',
+        padding: 12,
+        alignContent: 'space-between',
+        width: 350,
+        marginTop: 15,
+    },
+    titleAltura: {
+        fontSize: 20,
+        fontWeight: '600',
+        padding: 12,
+        textAlign: 'left',
+    },
+    textInputPeso: {
+        justifyContent: 'flex-start',
+        marginLeft: 5,
+        width: 120,
+    },
+    seccioBoto: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 12,
+        alignContent: 'space-between',
+        width: 350,
+        marginTop: 34,
+    },
+    titlePeso: {
+        fontSize: 20,
+        fontWeight: '600',
+        padding: 12,
+        textAlign: 'left',
+    },
+    textInputAltura: {
+        justifyContent: 'flex-start',
+        marginLeft: 5,
+        width: 120,
+    },
+    seccioPeso: {
+        flexDirection: 'row',
+        width: 90,
+        alignContent: 'space-around',
+    },
+    peso: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 12,
+        alignContent: 'space-between',
+        width: 350,
+    },
+    altura: {
+    },
+    seccio1: {
+        flex: 0.15,
+        fontSize: 12,
+        fontWeight: '600',
+        margin: 4,
+        padding: 12,
+        textAlign: 'right',
+    },
+    seccio2: {
+        flex: 0.5,
+        fontSize: 12,
+        fontWeight: '600',
+        margin: 4,
+        padding: 12,
+        height: 50,
+        textAlign: 'center',
+    },
+    seccio3: {
+        flex: 0.15,
+        fontSize: 12,
+        fontWeight: '600',
+        margin: 4,
+        padding: 12,
+        textAlign: 'left',
+    },
+    sectionTitle: {
+        fontSize: 35,
+        color: 'blue',
+        fontWeight: '600',
+        padding: 12,
+        textAlign: 'center',
+    },
+    button: {
+        borderRadius: 0,
+        color: 'white',
+        width: 300,
+        height: 40,
+    },
+    resultatIMC: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    IMC: {
+        fontSize: 25,
     }
 });
 
